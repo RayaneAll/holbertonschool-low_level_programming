015fc3b10012aa06188bc5d183822eb3a30aad47
RayaneAll <9975@holbertonstudents.com>
2024-10-21 13:51:31 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	printf("sizeof(int)       = %d\n",sizeof(int));
+	printf("sizeof(float)     = %d\n",sizeof(float));
+	printf("sizeof(double)    = %d\n",sizeof(double));
+	printf("sizeof(char)      = %d\n",sizeof(char));
+	return (0);
+}
