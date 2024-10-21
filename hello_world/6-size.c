1ef15f0e7dbe58822fb3ee8bbc7526318e84e216
RayaneAll <9975@holbertonstudents.com>
2024-10-21 14:05:59 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	printf("sizeof(char) = %d\n", sizeof(char));
-	printf("sizeof(int) = %d\n", sizeof(int));
-	printf("sizeof(long int) = %d\n", sizeof(long int));
-	printf("sizeof(long long int) = %d\n", sizeof(long long int));
-	printf("sizeof(float) = %d\n", sizeof(float));
+	printf("sizeof(char) = %lu\n", sizeof(char));
+	printf("sizeof(int) = %lu\n", sizeof(int));
+	printf("sizeof(long int) = %lu\n", sizeof(long int));
+	printf("sizeof(long long int) = %lu\n", sizeof(long long int));
+	printf("sizeof(float) = %lu\n", sizeof(float));
 	return (0);
 }
