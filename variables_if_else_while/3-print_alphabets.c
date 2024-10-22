3419cccafd28d5d4a56523240fa3e99af7243ac9
RayaneAll <9975@holbertonstudents.com>
2024-10-22 14:50:32 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,17 +12,19 @@ int main(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	char alphabetM[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
 	while (alphabet[i] != '\0')
 	{
 		putchar(alphabet[i]);
 		i++;
-		while (alphabetM[i] != '\0')
-		{
-			putchar(alphabetM[i]);
-			i++;
-		}
+	}
+	while (alphabetM[j] != '\0')
+	{
+		putchar(alphabetM[j]);
+		j++;
 	}
 	putchar('\n');
 	return (0);
