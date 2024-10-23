bf0900ba62efbbea0d8bed14dcf092319b7a12a6
RayaneAll <9975@holbertonstudents.com>
2024-10-23 13:23:07 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "zyxwvutsrqponmlkjihgfedcba";
+	int i;
+
+	i = 0;
+	while (alphabet[i] != '\0')
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
