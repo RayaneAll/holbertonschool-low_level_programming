f8a2a7b6c953e94fe035cc56dbd1dd78c74a0aaa
RayaneAll <9975@holbertonstudents.com>
2024-10-23 11:27:46 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("0");
+	printf("1");
+	printf("2");
+	printf("3");
+	printf("4");
+	printf("5");
+	printf("6");
+	printf("7");
+	printf("8");
+	printf("9\n");
+}
