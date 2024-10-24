72d2fc5278e23a712d7e3c051affcb4c5f14ca71
RayaneAll <9975@holbertonstudents.com>
2024-10-24 07:03:14 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
