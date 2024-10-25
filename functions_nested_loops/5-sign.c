a5943eb57e221d7cb6f49b4955400bc69d1c4ca9
RayaneAll <9975@holbertonstudents.com>
2024-10-25 11:31:16 +0000

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime alphabet en minucscule, suivi d'un saut de ligne
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
