fbe884586a529ead86daaf9bef6a2d084b1c6dfd
RayaneAll <9975@holbertonstudents.com>
2024-10-25 07:52:40 +0000

    1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-int main(void);
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
