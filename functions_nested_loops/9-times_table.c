229a56aa8484e97ca405ddf4e5967e51aa848ff4
RayaneAll <9975@holbertonstudents.com>
2024-10-25 14:50:33 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -25,7 +25,7 @@ void times_table(void)
 				_putchar('0' + (product / 10));
 				_putchar('0' + (product % 10));
 			}
-			if (j < 9)
+			else if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
