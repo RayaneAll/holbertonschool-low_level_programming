8efca229e4af9bea9ced3b6eb15d547bd75fd6f6
RayaneAll <9975@holbertonstudents.com>
2024-10-25 14:53:10 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ void times_table(void)
 	int j;
 	int product;
 
+	product = i * j;
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			product = i * j;
 
 			if (product < 10)
 			{
