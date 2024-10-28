d6c33f7fbfa4058d24246cfe221ae37a584cd1dd
RayaneAll <9975@holbertonstudents.com>
2024-10-28 12:55:29 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,21 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 15; j++)
-		{
-			_putchar(j + '0');
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 15; j++)
+        {
+            if (j < 10)
+                _putchar(j + '0');
+            else
+            {
+                _putchar((j / 10) + '0');
+                _putchar((j % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
