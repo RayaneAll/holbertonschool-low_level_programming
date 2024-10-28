28ab304ab15abbd45bc099cb21ca1acc6a7e703b
RayaneAll <9975@holbertonstudents.com>
2024-10-28 08:55:53 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - imprime alphabet en minucscule, suivi d'un saut de ligne
+ * _isupper - checks if a character is uppercase.
+ *
+ * @c: the character to check.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
 
 int _isupper(int c)
