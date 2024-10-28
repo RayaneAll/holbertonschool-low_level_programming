85357ba286726409686e869a4e415dfdfad76434
RayaneAll <9975@holbertonstudents.com>
2024-10-28 15:16:10 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,18 +5,14 @@
  * @n : variable for line
  *
  * Descritpion : multiply variable a and variable b
- * Return: return 0
  */
 
 void print_line(int n)
 {
-	if (n > 0)
+	while (n > 0)
 	{
-		_putchar(n * 95);
-		_putchar('\n');
+		_putchar('_');
+		n--;
 	}
-	else if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
