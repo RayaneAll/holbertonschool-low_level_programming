ff8b4e681331b7d7c5624a9afc8b80eea7d0b2d3
RayaneAll <9975@holbertonstudents.com>
2024-10-28 09:58:52 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Descritpion : print numbers
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
