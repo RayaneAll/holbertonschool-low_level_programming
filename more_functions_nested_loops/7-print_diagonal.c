06da631118f4bea55fe0015a926ca6e8792215d7
RayaneAll <9975@holbertonstudents.com>
2024-10-28 16:12:30 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mult - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar(' ');
+		n--;
+	}
+	_putchar(92);
+	_putchar('\n');
+}
