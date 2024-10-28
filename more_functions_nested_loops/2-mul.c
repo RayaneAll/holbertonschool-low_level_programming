85e4fe46c10bdae21751354821e97ec60c413754
RayaneAll <9975@holbertonstudents.com>
2024-10-28 09:10:48 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mult - multiplie a par b.
+ *
+ * a et b: les nombres a multiplier.
+ *
+ * Return: multiplication de a par b.
+ */
+
+int mult(int a, int b)
+{
+	return (a * b);
+}
