b41e36756a52dc9097c7e5af96337ffea01565d4
RayaneAll <9975@holbertonstudents.com>
2024-10-28 08:53:16 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime alphabet en minucscule, suivi d'un saut de ligne
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
