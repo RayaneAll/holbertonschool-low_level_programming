25bad70f352b8365fea10333cb0062001c940214
RayaneAll <9975@holbertonstudents.com>
2024-10-29 16:02:39 +0000

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * _puts - multiply numbers
+ * @str : bien
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
