f649465b8cc5ea5c5b48dad9c0f6204227d09b82
RayaneAll <9975@holbertonstudents.com>
2024-10-29 11:26:33 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - multiply numbers
+ * @n : betty
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
