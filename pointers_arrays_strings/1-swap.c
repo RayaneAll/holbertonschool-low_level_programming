9f8c2147bc18cb5f53ae4953619c2535e7e00aba
RayaneAll <9975@holbertonstudents.com>
2024-10-29 11:31:17 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
