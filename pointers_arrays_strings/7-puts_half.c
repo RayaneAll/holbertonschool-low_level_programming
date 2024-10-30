076ab4e2f9c8dcd11d02bb14a312f63b4f374b5f
RayaneAll <9975@holbertonstudents.com>
2024-10-30 16:11:03 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,13 +17,13 @@ void puts_half(char *str)
 	{
 		i++;
 	}
+	if (i % 2 != 0)
+	{
+		i--;
+	}
 	i = i / 2;
 	while (str[i] != '\0')
 	{
-		if (i % 2 != 0)
-		{
-			i--;
-		}
 		_putchar(str[i]);
 		i++;
 	}
