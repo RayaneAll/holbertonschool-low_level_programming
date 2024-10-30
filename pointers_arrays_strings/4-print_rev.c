56bd9c91d3bc9de19bd8c3ee3c99bbf393ce1a6c
RayaneAll <9975@holbertonstudents.com>
2024-10-30 08:28:42 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - multiply numbers
+ * @s : betty
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	_putchar(s[i]);
+	_putchar('\n');
+}
