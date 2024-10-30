03ff6002819c2989f39ed70941c42ef57ddb18d2
RayaneAll <9975@holbertonstudents.com>
2024-10-30 09:31:08 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - multiply numbers
+ * @s : bien
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (s[i] != s[0])
+	{
+		i--;
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
