93f1536298f27c29d33139a5611ab971345f5f4a
RayaneAll <9975@holbertonstudents.com>
2024-10-30 12:21:06 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,18 @@
 void rev_string(char *s)
 {
 	int i;
+	int j;
 
 	i = 0;
+	j = i - 1;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	_putchar('\n');
-	while (s[i] != s[0])
+	while (s[j] != s[0])
 	{
-		i--;
+		j++;
 	}
 	_putchar('\n');
 }
