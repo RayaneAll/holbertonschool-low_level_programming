5b893c196f1c206ecce97f49cae90531082fc11e
RayaneAll <9975@holbertonstudents.com>
2024-10-30 13:21:20 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - multiply numbers
+ * @str : betty
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
