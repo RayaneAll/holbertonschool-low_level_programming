2f1966ef4f4288ae24295e644b0b736dcc3983d6
RayaneAll <9975@holbertonstudents.com>
2024-10-31 08:01:53 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - multiply numbers
+ * @a : variable for the first number
+ * @n : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < a[n]; i++)
+	{
+		printf("%d, ", a[n]);
+	}
+}
