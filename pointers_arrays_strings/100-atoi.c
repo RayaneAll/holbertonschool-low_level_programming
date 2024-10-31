ec7be4c2f621e7c45cf92c5c9626434b53fd64da
RayaneAll <9975@holbertonstudents.com>
2024-10-31 12:09:11 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - multiply numbers
+ * @s : variable for the first number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int _atoi(char *s)
+{
+	int resultat;
+	int i;
+	int positif;
+
+	resultat = 0;
+	i = 0;
+	positif = 1;
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	while (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+		{
+			positif = positif * -1;
+		}
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		resultat = resultat * 10 + (*s - '0');
+		i++;
+	}
+	resultat = resultat * positif;
+	return (resultat);
+}
