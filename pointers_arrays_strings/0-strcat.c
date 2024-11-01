924e4156cf5a79023d0029e12c164a8b730c767d
RayaneAll <9975@holbertonstudents.com>
2024-11-01 15:23:21 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - multiply letters
+ *
+ * @s : variable for the first number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcat - multiply letters
+ *
+ * @dest : variable for the first number
+ * @src : definition
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len;
+	int i;
+
+	dest_len = _strlen(dest);
+	i = 0;
+	while (src[i] = '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	resturn(dest);
+}
