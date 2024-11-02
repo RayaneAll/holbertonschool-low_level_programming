74d7fab2736251abcd10f6828ee62e684adfc39c
RayaneAll <9975@holbertonstudents.com>
2024-11-02 11:25:08 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,20 +14,13 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] || s2[i])
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (1);
+			return (s1[i] - s2[i]);
 		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else
-		{
-			i++;
-		}
+		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
