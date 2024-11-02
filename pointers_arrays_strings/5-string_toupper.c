9d2dc1342f205b61d0711c2e6c6916acb57852e9
RayaneAll <9975@holbertonstudents.com>
2024-11-02 12:13:22 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * string_toupper - multiply numbers
+ * @str : variable for the first number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
