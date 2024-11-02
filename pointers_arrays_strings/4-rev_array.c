ee0d67d0d541e484cdb43de0bf3a99a3081afecb
RayaneAll <9975@holbertonstudents.com>
2024-11-02 11:48:13 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - multiply numbers
+ * @a : variable for the first number
+ * @n : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	i = 0;
+	temp = 0;
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
