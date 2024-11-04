b7eb8ebb0a6e0fc0c13a27ee6e2910b7b4ca9f51
RayaneAll <9975@holbertonstudents.com>
2024-11-04 14:18:42 +0000

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mult - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
