a74391273332de9365e718ed927c32277e56f8fc
RayaneAll <9975@holbertonstudents.com>
2024-11-06 14:58:02 +0000

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,12 +10,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	return (i);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
