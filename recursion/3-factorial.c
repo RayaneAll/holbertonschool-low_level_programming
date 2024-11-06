036f22de791cf4e92b21ce22bde0edd32dba2715
RayaneAll <9975@holbertonstudents.com>
2024-11-06 15:10:27 +0000

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * mult - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
