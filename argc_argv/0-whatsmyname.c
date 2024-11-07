1fbd84e9f7c2e1058f770870a8275519ac246855
RayaneAll <9975@holbertonstudents.com>
2024-11-07 15:28:19 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mult - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	puts(argv[0]);
+	return (0);
+}
