7406c57eb875959fa273d575bb2e4e4479183dc4
RayaneAll <9975@holbertonstudents.com>
2024-11-07 15:52:17 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiply numbers
+ * @argc : variable for the first number
+ * @argv : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
