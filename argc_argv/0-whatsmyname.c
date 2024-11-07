585c57b7db61d561f9d27c863fc76a9f1130da3f
RayaneAll <9975@holbertonstudents.com>
2024-11-07 15:30:39 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * mult - multiply numbers
- * @a : variable for the first number
- * @b : variable for the seconde number
+ * main - multiply numbers
+ * @argc : variable for the first number
+ * @argv : variable for the seconde number
  *
  * Descritpion : multiply variable a and variable b
  * Return: return 0
