f06508a5ba0df5c1cc4ff901f7ead8c970136783
RayaneAll <9975@holbertonstudents.com>
2024-11-12 13:14:46 +0000

    a

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - multiply numbers
+ * @size : variable for the first number
+ * @c : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * sizeof(char) + 1);
+	if (tab == 0)
+	{
+		return (0);
+	}
+	if (size == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		tab[i] = c;
+	}
+	return (tab);
+	free(tab);
+}
