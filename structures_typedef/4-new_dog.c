d40dbcb5651344dcd8915a0827e8b45fe9c43592
RayaneAll <9975@holbertonstudents.com>
2024-11-15 16:14:30 +0100

    enfin

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - multiply numbers
+ * @str : variable for the first number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int _strlen(char *str)
+{
+int length = 0;
+while (str[length] != '\0')
+length++;
+return (length);
+}
+
+/**
+ * _strcpy - multiply numbers
+ * @dest : variable for the first number
+ * @src : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
 
 /**
  * new_dog - multiply numbers
@@ -18,21 +54,21 @@ dog_t *dog;
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
-dog->name = malloc(strlen(name) + 1);
+dog->name = malloc(_strlen(name) + 1);
 if (dog->name == NULL)
 {
 free(dog);
 return (NULL);
 }
-strcpy(dog->name, name);
-dog->owner = malloc(strlen(owner) + 1);
+_strcpy(dog->name, name);
+dog->owner = malloc(_strlen(owner) + 1);
 if (dog->owner == NULL)
 {
 free(dog->name);
 free(dog);
 return (NULL);
 }
-strcpy(dog->owner, owner);
+_strcpy(dog->owner, owner);
 dog->age = age;
 return (dog);
 }
