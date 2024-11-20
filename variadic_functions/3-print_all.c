12a2bdd85f809db5134423e2fba66b6e2cf6cdc7
RayaneAll <9975@holbertonstudents.com>
2024-11-20 15:32:54 +0100

    Il est lent ce lait

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - multiply numbers
@@ -12,44 +11,39 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	int printed = 0;
+va_list args;
+unsigned int i = 0;
+char *str, *separator = "";
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (format && format[i] != '\0')
-	{
-		if (printed)
-			printf(", ");
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", separator, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", separator, str);
+break;
+default:
+i++;
+continue;
+}
+separator = ", ";
+i++;
+}
 
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			break;
-		default:
-			i++;
-			continue;
-		}
-		printed = 1;
-		i++;
-	}
-
-	printf("\n");
-	va_end(args);
+va_end(args);
+printf("\n");
 }
