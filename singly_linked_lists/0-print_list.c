45830a44f3e36a28187dc9d542e491a81479294b
RayaneAll <9975@holbertonstudents.com>
2024-12-03 13:27:27 +0100

    Ba Erwan il est nul quoi

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,18 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
 while (h != NULL)
 {
 	if (h->str == NULL)
 	{
-	printf("[0] (nil)\n")
+	printf("[0] (nil)\n");
 	}
 else
 {
-printf("[%u] %s\n", h->len, h->str)
+printf("[%u] %s\n", h->len, h->str);
 }
-count++
+count++;
 h = h->next;
 }
 return (count);
