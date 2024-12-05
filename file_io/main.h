4ebdecc97fe7eab2dede31ee117a1cf946e2e1a7
RayaneAll <9975@holbertonstudents.com>
2024-12-05 15:51:33 +0100

    Correction of file main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,11 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* __MAIN_H__ */
+#endif /* MAIN_H */
